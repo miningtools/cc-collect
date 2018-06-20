d52f201278c71bb55106e9447496f5a68130c50c
miningtools <cminingtools@gmail.com>
2018-06-20 12:14:52 +0200

    .

--- config/config.test.js
+++ config/config.test.js
@@ -4,7 +4,7 @@ var config = require('./config.global');
 config.env = 'test';                     // dev || test || prod
 
 config.log_level = 'debug';             // error || warn || info || debug || silly
-config.log_mode = 'console';            // console || file || both
+config.log_mode = 'both';            // console || file || both
 
 
 module.exports = config;
